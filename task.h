5b86f26eb6d6b63b8c7e303e2fc3a3e1bd53b232
Omar Khaled <102737659+OmarKhaled2005@users.noreply.github.com>
2023-11-15 21:37:18 +0200

    Header update.h

--- task.h
+++ task.h
@@ -1,21 +1,78 @@
-#ifndef MACHINE_LANGUAGE_SIMULATOR_H
-#define MACHINE_LANGUAGE_SIMULATOR_H
+#ifndef MACHINE_SIMULATOR_HEADER_H
+#define MACHINE_SIMULATOR_HEADER_H
 
-#include <stdint.h>
+//libraries included in the code
+#include <bitset>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
 
-// Define the opcodes for different instructions
-#define OP_LOAD_MEM  0x1
-#define OP_LOAD_VAL  0x2
-#define OP_STORE_MEM 0x3
-#define OP_STORE_SCR 0x3
-#define OP_MOVE      0x4
-#define OP_ADD_INT   0x5
-#define OP_ADD_FLOAT 0x6
-#define OP_JUMP      0xB
-#define OP_HALT      0xC
+//Classes used in the code
+class MainMemory {
+private:
+  int size;
+  std::vector<std::string> memory;
 
-// Define the registers and memory size
-#define NUM_REGISTERS 16
-#define MEMORY_SIZE   256
+public:
+  MainMemory();
+  void store(int address, std::string value);
+  std::string read(int address);
+};
 
-#endif // MACHINE_LANGUAGE_SIMULATOR_H
+class Registers {
+private:
+  int size;
+  std::vector<std::string> memory;
+
+public:
+  Registers();
+  void store(int address, std::string value);
+  std::string read(int address);
+};
+
+class Instructions {
+public:
+  void execute(std::string s, Registers &cpuRegister, MainMemory &ram,
+               int &programC, std::vector<std::string> &screen);
+};
+
+class CPU {
+private:
+  int programCounter;
+  std::string instructionRegister;
+  std::vector<std::string> screen;
+  Instructions xdd;
+
+  void fetch(MainMemory &r);
+
+public:
+  CPU();
+  void runProgram(MainMemory &ram, Registers &cpuRegister);
+  std::string getIR();
+  int getPC();
+  void getScreen();
+};
+
+class Machine : CPU {
+public:
+  MainMemory ram;
+  Registers cpuRegister;
+  CPU main;
+  std::vector<std::string> linesAfter;
+
+  Machine();
+  void loadFile();
+  void printData(std::string userInput);
+};
+
+// Prototype for helping functions
+std::string hexToBinary(const std::string &hexString);
+std::string hexToBinary4(const std::string &hexString);
+unsigned long binaryToInt(const std::string &binaryString);
+unsigned long binaryToInt4(const std::string &binaryString);
+std::string twosComplementAddition(std::string valueX, std::string valueY);
+std::string decimalToHex(const int &decimalInteger);
+
+#endif // MACHINE_SIMULATOR_HEADER_H
